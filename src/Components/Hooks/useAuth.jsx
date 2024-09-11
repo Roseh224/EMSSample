a9a8e8575fe37b317a9c15c7431b8e53ee205103
Rose Hudson <47463061+Roseh224@users.noreply.github.com>
2024-09-11 13:43:04 -0600

    Deleted src/Components/Hooks directory

--- src/Components/Hooks/useAuth.jsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../Components/context/AuthProvider";
-
-export default function useAuth () {
-    return useContext(AuthContext);
-}
